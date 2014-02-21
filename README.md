4c77a46c4c310fd95b7c4d6368dc0b506d9407cf
Nick Baker <nbaker@pentaho.com>
2014-02-21 14:46:39 -0500

    Allowing for the case where FragmentWatcher is installed after orphaned Fragments. It will inspect existing bundles upon startup and restart hosts as needed.

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ OSGI Fragments are a useful tool for adding capabilities to legacy libraries whi
 Fragment Watcher will watch for any Fragment Bundle being installed. It then checks to see if the Host Bundle for that fragment is already installed and resolved in the system. If so, the host will be refreshed to allow the fragment to attach.
 
 ### Usage
-Ideally the FragmentWatcher bundle should be installed and started before any fragments and hosts. If this is not possible, extending the FragmentWatcher to inspect already installed bundles is possible though not in the scope of my initial usage: http://www.osgi.org/javadoc/r4v42/org/osgi/service/packageadmin/PackageAdmin.html#getFragments(org.osgi.framework.Bundle)
+FragmentWatcher can be installed early or late. If installed early, the EventAdmin portion will watch new bundles as they're installed. If it is installed late (after fragments and hosts potentially) it will inspect all existing bundles thru PackageAdmin to detect orphaned Fragments.
--- src/main/java/org/codeoncoffee/fragment/FragmentWatcher.java
+++ src/main/java/org/codeoncoffee/fragment/FragmentWatcher.java
@@ -8,6 +8,7 @@ import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
 import org.osgi.service.log.LogService;
+import org.osgi.service.packageadmin.PackageAdmin;
 
 import java.util.Dictionary;
 import java.util.Hashtable;
@@ -27,14 +28,29 @@ public class FragmentWatcher implements BundleActivator, EventHandler {
 
   @Override public void start( BundleContext bundleContext ) throws Exception {
     this.bundleContext = bundleContext;
+    // get the LogService
+    logService = bundleContext.getService( bundleContext.getServiceReference( LogService.class ) );
+
+    // Check for already loaded fragments that failed to attach to their hosts
+    // PackageAdmin is deprecated, but I cannot find how to replicate this code with BundleWiring apis
+    PackageAdmin pAdmin = bundleContext.getService( bundleContext.getServiceReference( PackageAdmin.class ) );
+    final Bundle[] bundles = bundleContext.getBundles();
+    for ( Bundle bundle : bundles ) {
+      final String host = bundle.getHeaders().get( "Fragment-Host" );
+      if ( host != null && pAdmin.getHosts( bundle ) == null ) {
+        restartHost( host );
+      }
+    }
+
+
+    // Register this instance as an EventHandler
     String[] topics = new String[] {
       "org/osgi/framework/BundleEvent/INSTALLED"
     };
-
     Dictionary<String, Object> props = new Hashtable<String, Object>();
     props.put( EventConstants.EVENT_TOPIC, topics );
     bundleContext.registerService( EventHandler.class.getName(), this, props );
-    logService = bundleContext.getService( bundleContext.getServiceReference( LogService.class ) );
+
   }
 
   @Override public void stop( BundleContext bundleContext ) throws Exception {
@@ -47,20 +63,24 @@ public class FragmentWatcher implements BundleActivator, EventHandler {
     final String host = bundle.getHeaders().get( "Fragment-Host" );
     if ( host != null ) {
       logService.log( LogService.LOG_INFO, "Fragment detected for host: " + host );
-      try {
-        final Bundle[] bundles = bundleContext.getBundles();
-        for ( Bundle b : bundles ) {
-          if ( host.equals( b.getSymbolicName() ) && b.getState() > Bundle.INSTALLED) {
-            b.update();
-            logService.log( LogService.LOG_INFO, "Host: " + host + " reloaded to attach fragment" );
-            return;
-          }
+      restartHost( host );
+    }
+  }
+
+  private void restartHost( String host ) {
+    try {
+      final Bundle[] bundles = bundleContext.getBundles();
+      for ( Bundle b : bundles ) {
+        if ( host.equals( b.getSymbolicName() ) && b.getState() > Bundle.INSTALLED ) {
+          b.update();
+          logService.log( LogService.LOG_INFO, "Host: " + host + " reloaded to attach fragment" );
+          return;
         }
-      } catch ( BundleException e ) {
-        logService.log( LogService.LOG_ERROR, "Bundle Exception trying to find Fragment-Host to restart", e );
-      } catch ( Exception e ) {
-        logService.log( LogService.LOG_ERROR, "Unknown Error trying to find Fragment-Host to restart", e );
       }
+    } catch ( BundleException e ) {
+      logService.log( LogService.LOG_ERROR, "Bundle Exception trying to find Fragment-Host to restart", e );
+    } catch ( Exception e ) {
+      logService.log( LogService.LOG_ERROR, "Unknown Error trying to find Fragment-Host to restart", e );
     }
   }
 }
